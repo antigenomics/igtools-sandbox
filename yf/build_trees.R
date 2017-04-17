04061155afd73c55ed40e2cfb6d6d9db1e18c2b4
Anna <anna@localhost.localdomain>
2017-04-17 19:34:05 +0300

    Filter novel shm

--- yf/build_trees.R
+++ yf/build_trees.R
@@ -1,11 +1,14 @@
+# to do: add positions for cdr3 mutations
+
 library(stringr)
 library(dplyr)
 library(fitdistrplus)
 library(doParallel)
-library(foreach)
+library(foreach) 
 library(igraph)
 library(muscle)
 library(Biostrings)
+library(tidyr)
 
 find_kmers <- function(string, k=5){
   n <- nchar(string) - k + 1
@@ -21,15 +24,6 @@ interleave <- function(v1, v2){
   return(z)
 }
 
-extract_mut <- function(mutations){
-  #delete position from mutation record
-  mut_type <- sapply(mutations, function(x) str_extract(x, '[SID]'))
-  subs <- sapply(mutations[which(mut_type == 'S')], function(x) str_extract(x, '[>TAGC]+'))
-  ins <- sapply(mutations[which(mut_type == 'I')], function(x) paste0('I', nchar(str_extract(x, '[TAGC]+'))))
-  dels <- sapply(mutations[which(mut_type == 'D')], function(x) paste0('D', nchar(str_extract(x, '[TAGC]+'))))
-  unlist(c(subs, ins, dels))
-}
-
 mutations_weight <- function(mutations){
   prod(merge(data.frame(mutation = mutations), frequencies)$freq)
 }
@@ -42,7 +36,7 @@ merge_gaps <- function(mat, gaps, i){
   if (indel.num > 0){
     for (x in 1:indel.num){
       v2 <- c( v2[0:(gaps[[i]][x,'start']-1)], gaps[[i]][x,'end']-gaps[[i]][x,'start']+1, 
-                 v2[(gaps[[i]][x,'end']+1):length(v2)] )
+               v2[(gaps[[i]][x,'end']+1):length(v2)] )
       v1 <- c( v1[0:(gaps[[i]][x,'start']-1)], '-', v1[(gaps[[i]][x,'end']+1):length(v1)] )
       gaps[[i]] <- gaps[[i]] - (gaps[[i]][x,'end']-gaps[[i]][x,'start'])
     }
@@ -64,20 +58,20 @@ get_mutations <- function(s1, s2){
   DNAset = DNAStringSet(c(s1, s2))
   alignment <- unmasked(muscle(DNAset, quiet = TRUE))
   aln.table <- align2table(alignment)
-
+  
   aln.table %>% mutate(s1 = as.character(s1), s2 = as.character(s2)) %>%
     filter(s1 != s2) %>%
     mutate(n1 = ifelse(s1 == '-', paste0('D', s2), 
-                         ifelse(s2 == '-', paste0('I', s1), paste(s1, s2, sep='>'))),
+                       ifelse(s2 == '-', paste0('I', s1), paste(s1, s2, sep='>'))),
            n2 = ifelse(s2 == '-', paste0('D', s1), 
-                         ifelse(s1 == '-', paste0('I', s2), paste(s2, s1, sep='>')))) %>%
+                       ifelse(s1 == '-', paste0('I', s2), paste(s2, s1, sep='>')))) %>%
     dplyr::select(-s1, -s2)
 }
 
 define_parent <- function(mut.table){
   score1 <- mutations_weight(mut.table$n1)
   score2 <- mutations_weight(mut.table$n2)
-
+  
   parent = ifelse(score1 > score2, 'n1', 'n2')
   list(parent = parent, mutations=dplyr::select(cdr3.muts, -get(parent))[,1])
 }
@@ -111,7 +105,7 @@ pair_compare <- function(i, gm = 'yes', alpha = 1e-08){
             n1 = ifelse(length(shared.muts) == length(mut.i),i, j)
             n2 = ifelse(n1 == i, j, i)
             cdr3.muts <- list(get_mutations(df$cdr3nt[n1], df$cdr3nt[n2])$n2)
-            muts.between = list(extract_mut(setdiff(df$all.mutations[[n2]], df$all.mutations[[n1]])))
+            muts.between = list(setdiff(df$all.mutations[[n2]], df$all.mutations[[n1]]))
             
             cdr3.muts.lst[[paste(n1,n2,sep='_')]] <- cdr3.muts
             non.cdr3.muts.lst[[paste(n1,n2,sep='_')]] <- muts.between
@@ -175,14 +169,14 @@ clone_info <- function(tree){
     diameter = max(mut.from.root)
     mean.mut = mean(mut.from.root)
     total.mut = sum(E(tree)$weight)
-  
+    
     branching = length(leaves)/mean.mut
     mean.degree = mean(degree(tree))
     
     clone <- rbind(clone, list(root = root, cdr3nt = cdr3nt, freq = freq, freq.sum = freq.sum, 
-                   leaves = length(leaves), nodes = length(V(tree)), root.mut = root.mut, 
-                   diameter = diameter, mean.mut = mean.mut, total.mut = total.mut, 
-                   branching = branching, mean.degree = mean.degree))
+                               leaves = length(leaves), nodes = length(V(tree)), root.mut = root.mut, 
+                               diameter = diameter, mean.mut = mean.mut, total.mut = total.mut, 
+                               branching = branching, mean.degree = mean.degree))
   }
   return(clone)
 }
@@ -190,17 +184,17 @@ clone_info <- function(tree){
 make_cytoscape_files <- function(edge.table){
   #write net file
   net <- data.frame(from = edge.table$n1, to = edge.table$n2, interaction = rep('shm', nrow(edge.table)))
-  write.table(net, file=paste0('~/yf/trees/', sample, '.net.txt'), sep='\t', row.names=FALSE, quote=FALSE)
-
+  write.table(net, file=paste0('trees/', sample, '.net.txt'), sep='\t', row.names=FALSE, quote=FALSE)
+  
   # write edge file
   edge <- data.frame(edge = paste(edge.table$n1, '(shm)', edge.table$n2, sep=' '), mutations = edge.table$mutations)
-  write.table(edge, file=paste0('~/yf/trees/', sample, '.edge.txt'), sep='\t', row.names=FALSE, quote=FALSE)
-
+  write.table(edge, file=paste0('trees/', sample, '.edge.txt'), sep='\t', row.names=FALSE, quote=FALSE)
+  
   # write node file
   all.nodes <- unique(c(edge.table$n1, edge.table$n2))
   node <- df[all.nodes,] %>% dplyr::select(cdr3aa, v, j, freq)
   node$node <- all.nodes
-  write.table(node, file=paste0('~/yf/trees/', sample, '.node.txt'), sep='\t', row.names=FALSE, quote=FALSE)
+  write.table(node, file=paste0('trees/', sample, '.node.txt'), sep='\t', row.names=FALSE, quote=FALSE)
 }
 
 
@@ -219,9 +213,12 @@ young <- paste0('yf_young_RNA/', c("Antipyat", "Epifancev", "Hadjibekov", "Koshk
 cl <- makeCluster(detectCores())
 registerDoParallel(cl)
 
+new.shm <- data.frame()
+cdr3.shm <- data.frame()
+
 for (sample in c(old, young)){
   #prepare dataset
-  df <- read.table(paste0('~/yf/', sample, '.clones.txt'), header=T, sep="\t")
+  df <- read.table(paste0('data/', sample, '.clones.txt'), header=T, sep="\t")
   df <- df %>% mutate(all.mutations=paste(mutations.nt.FR1, mutations.nt.CDR1, mutations.nt.FR2, mutations.nt.CDR2,
                                           mutations.nt.FR3, mutations.nt.CDR3, mutations.nt.FR4, sep=','),
                       cdr3nt = as.character(cdr3nt)) %>%
@@ -255,7 +252,33 @@ for (sample in c(old, young)){
   
   clones <- rbind(clones, singletons)
   
-  write.table(clones, file=paste0('~/yf/trees/stat/', sample, '.txt'), sep='\t', row.names=FALSE, quote=FALSE)
+  write.table(clones, file=paste0('trees/stat/', sample, '.txt'), sep='\t', row.names=FALSE, quote=FALSE)
+  
+  #filter shm table
+  type = str_extract(sample, '.NA')
+  patient = str_sub(str_extract(sample, '\\/(.+)'), 2)
+  proj = str_sub(str_extract(sample, 'yf_[^_]+_'), 4, -2)
+    
+  .new.shm <- dplyr::select(final.pairs, n2, non.cdr3.muts)
+  .new.shm$pos <- sapply(.new.shm$non.cdr3.muts, function(x) str_extract(unlist(x), '\\d+'))
+  .new.shm <- .new.shm %>% unnest(pos = pos) %>% mutate(type = type, sample = patient)
+  new.shm <- rbind(new.shm, .new.shm)
+  
+  .cdr3.shm <- final.pairs[sapply(final.pairs$cdr3.muts, function(x) length(x[[1]])) > 0,]
+  .cdr3.shm <- mutate(.cdr3.shm, proj=proj, sample=sample, type=type, replica=1, allele.rate=0,
+                      allele.clonotype.share=0, clonotype.id=n2, pos.nt=NA, segment.name=NA,
+                      region='CDR3')
+  cdr3.shm <- rbind(cdr3.shm, .cdr3.shm)
 }
 
 stopCluster(cl)
+
+#preprocessed shm table
+load("sp.Rda")
+shm <- df
+
+new.shm <- transmute(new.shm, clonotype.id=n2-1, pos.nt=as.integer(pos), sample=sample, type=type)
+new.shm.1 <- merge(new.shm, shm, by=c('clonotype.id', 'pos.nt', 'sample', 'type'))
+df <- new.shm.1
+save(df, file='sp_new.Rda')
+
