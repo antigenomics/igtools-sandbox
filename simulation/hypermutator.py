f460005ace0d69314e1d5182dbbfc1908e5ab017
Anna <alcedooo@gmail.com>
2018-04-22 22:36:32 +0300

    Speed improvement

--- simulation/hypermutator.py
+++ simulation/hypermutator.py
@@ -5,73 +5,70 @@ import multiprocessing as mp
 
 class Clonotype:
 
-    def __init__(self, sequence, index, parent, mutations):
-        self.seq = sequence
-        self.i = index
-        self.p = parent
-        self.muts = mutations
-
-
-def mutate(cl, model):
-
-    mutations = []
-    new_seq = cl.seq
-    indels = []
-
-    for i in range(len(cl.seq)):
-        nucl = cl.seq[i]
-        new_nucl = np.random.choice(model['substitution.pattern'][nucl][0],
-                                        p = model['substitution.pattern'][nucl][1])
-        if new_nucl != nucl:
-            new_seq = new_seq[:i] + new_nucl + new_seq[i + 1:]
-            mutations.append('S' + str(i) + ':' + nucl + '>' + new_nucl)
-
-        indel = np.random.choice(model['indel.pattern'][0], p = model['indel.pattern'][1])
-        if indel != 0:
-            indels.append([i, indel])
-            mutations.append(('D' if indel < 0 else 'I') + str(i) + ':' + str(indel).strip('-'))
-
-    shift = 0
-    for i in indels:
-        if i[1] < 0:
-            new_seq = new_seq[:i[0] + shift] + new_seq[i[0] + i[1] + shift:]
-        else:
-            insertion = ''.join(map(str, np.random.choice(['A', 'C', 'G', 'T'], i[1])))
-            new_seq = new_seq[:i[0] + shift] + insertion + new_seq[i[0] + shift:]
-        shift += i[1]
-
-    if mutations:
-        return Clonotype(new_seq, 0, cl.i, mutations)
+    def __init__(self, seq, index, parent, muts, root, germline):
+        self.seq = seq
+        self.index = index
+        self.parent = parent
+        self.muts = muts
+        self.root = root
+        self.germline = germline
 
 
 def parse_model():
-
     f = open(args.model)
-    mr = f.read().strip('#').split('#')
-    f.close()
-    model = {}
-    for i in mr:
-        params = i.strip('\n').split('\n')
-        model[params[0]] = params[1:]
+    substitution_matrix = np.zeros((4, 4))
+    for i, line in enumerate(f):
+        substitution_matrix[i, :] = np.array([float(x) for x in line.split()])
+    return substitution_matrix
 
-    model['substitution.ratio'] = float(model['substitution.ratio'][0])
 
-    bases = ['A', 'C', 'G', 'T']
-    sp = {}
-    for i, n in enumerate(bases):
-        variant = [bases[i]] + bases[:i] + bases[i+1:]
-        probability = [1 - model['substitution.ratio']] + \
-                      [float(x) * model['substitution.ratio'] for x in model['substitution.pattern'][i].split()]
-        sp[n] = [variant, probability]
-    sp['N'] = [['N', 'A', 'C', 'G', 'T'], [1 - model['substitution.ratio']] + [model['substitution.ratio']/4] * 4]
+def seq_to_array(seq):
+    seq = np.array(list(seq))
+    array = np.zeros(len(seq), dtype = int)
+    array[seq == 'A'] = 0
+    array[seq == 'C'] = 1
+    array[seq == 'G'] = 2
+    array[seq == 'T'] = 3
+    return array
 
-    model['substitution.pattern'] = sp
 
-    model['indel.pattern'] = [x.split() for x in model['indel.pattern']]
-    model['indel.pattern'][0] = [int(x) for x in model['indel.pattern'][0]]
-    model['indel.pattern'][1] = [float(x) for x in model['indel.pattern'][1]]
+def array_to_seq(array):
+    seq = np.chararray(shape=array.shape)
+    seq[array == 0] = 'A'
+    seq[array == 1] = 'C'
+    seq[array == 2] = 'G'
+    seq[array == 3] = 'T'
+    return ''.join(seq)
 
-    return model
+
+def matrix_repr(seq):
+    matrix = np.zeros((seq.shape[0], 4))
+    matrix[np.arange(seq.shape[0]), seq] = 1
+    return matrix
+
+
+def random_choice_prob_index(a, axis=1):
+    r = np.expand_dims(np.random.rand(a.shape[1-axis]), axis=axis)
+    return (a.cumsum(axis=axis) > r).argmax(axis=axis)
+
+
+def mutate(cl, model):
+    np.random.seed()
+
+    substitution_probas = np.dot(matrix_repr(cl.seq), model)
+
+    new_seq = random_choice_prob_index(substitution_probas)
+    mut_idxs = np.where(new_seq != cl.germline.seq)[0]
+
+    if mut_idxs.shape[0] > 0:
+        return Clonotype(seq=new_seq, index=0, parent=cl.index, muts=mut_idxs, root=cl.root, germline=cl.germline)
+
+
+def write_mutations(cl):
+    old = array_to_seq(cl.germline.seq[cl.muts])
+    new = array_to_seq(cl.seq[cl.muts])
+    muts_str = [str(idx) + ':' + x + '>' + y for idx, x, y in zip(cl.muts, old, new)]
+    return muts_str
 
 
 parser = argparse.ArgumentParser(description="Introduce hypermutations into generated IGH sequences.")
@@ -84,13 +81,26 @@ args = parser.parse_args()
 # read SHM pattern
 m = parse_model()
 
-# read generated IGH repertoire
-r = open(args.seqs)
+# read generated IGH repertoire and introduce "old" mutations to the future tree roots
 rep = []
-for line in r.read().splitlines()[1:]:
-    index = int(line.split(';')[0]) + 1
+germlines = []
+
+r = open(args.seqs)
+r.readline()
+
+for line in r:
+    index = line.split(';')[0]
     seq = line.split(';')[1]
-    rep.append(Clonotype(seq, index, index, []))
+    seq = seq_to_array(seq)
+
+    g = Clonotype(seq, index, index, np.array([]), index, germline=None)
+    g.germline = g
+    germlines.append(g)
+
+    cl = mutate(g, m)
+    cl.index = index
+    cl.germline = g
+    rep.append(cl)
 
 # go through repertoire several times and introduce mutations
 for s in range(args.tree_height):
@@ -98,15 +108,16 @@ for s in range(args.tree_height):
     new_rep = [pool.apply(mutate, args=(cl, m)) for cl in rep]
     new_rep = [cl for cl in new_rep if cl is not None]
     for i, cl in enumerate(new_rep):
-        cl.i = len(rep) + 1 + i
+        cl.index = len(rep) + 1 + i
     rep = rep + new_rep
 
 # write results
-f1 = open('mutated_seqs.csv', 'w')
-f1.write( '\n'.join( ['{}:{}'.format(x.i, x.seq) for x in rep] ) )
+f1 = open('mutated_seqs_test.csv', 'w')
+
+f1.write( '\n'.join( ['{}:{}'.format(x.index, array_to_seq(x.seq)) for x in rep] ) )
 f1.close()
 
-f2 = open('mutated_seqs_info.csv', 'w')
-f2.write('index\tparent\tsequence\tmutations\n')
-f2.write( ''.join( ['{}\t{}\t{}\t{}\n'.format(x.i, x.p, x.seq, ','.join(x.muts)) for x in rep] ) )
+f2 = open('mutated_seqs_test_info.csv', 'w')
+f2.write('index\tparent\troot\tsequence\tmutations\n')
+f2.write( ''.join( ['{}\t{}\t{}\t{}\t{}\n'.format(x.index, x.parent, x.root, array_to_seq(x.seq), write_mutations(x)) for x in rep] ) )
 f2.close()
