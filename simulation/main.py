93f5cf36eaa2c9c3231eab95d5ab30326c37f5bd
Anna <alcedooo@gmail.com>
2018-08-04 22:01:38 +0300

    Run igor with custom templates

--- simulation/main.py
+++ simulation/main.py
@@ -5,14 +5,13 @@ from subprocess import Popen, PIPE, call
 from copy import deepcopy
 from scipy.stats import gamma
 from hypermutator import *
-#from sequencer import *
 
 
 class IgorError(Exception):
     pass
 
 
-def run_igor(size, model, dir, batch):
+def run_igor(size, model, dir, template):
     # Note: do not forget to add correct path to igor to PATH variable
 
     if model.startswith('bcr'):
@@ -25,10 +24,8 @@ def run_igor(size, model, dir, batch):
            "--noerr",
            "-set_wd", dir,
            "-chain", chain,
-           "-batch", batch,
-           "-set_custom_model", "/home/anna/simulation/models/igor/" + model + "_" + batch + "/model_parms.txt",
-           "/home/anna/simulation/models/igor/" + model + "_" + batch + "/model_marginals.txt",
-           "-species", "human"]
+           "-species", "human",
+           "-set_genomic", "--V", template]
 
     process = Popen(cmd, stdout=PIPE, stderr=PIPE)
     stdout, stderr = process.communicate()
@@ -43,18 +40,12 @@ def run_igor(size, model, dir, batch):
         raise IgorError('IGoR failed, look igor_log.txt for more information')
 
 
-def read_igor_output(dir, model):
+def read_igor_output(dir):
     s = pd.read_csv(dir + 'generated_seqs_noerr.csv', sep=';')
     r = pd.read_csv(dir + 'generated_realizations_noerr.csv', sep=';')
-    if model.startswith('bcr'):
-        v_col = 'GeneChoice_V_gene_Undefined_side_prio8_size97'
-        j_col = 'GeneChoice_J_gene_Undefined_side_prio7_size7'
-    else:
-        v_col = 'GeneChoice_V_gene_Undefined_side_prio7_size103'
-        j_col = 'GeneChoice_J_gene_Undefined_side_prio6_size68'
 
-    r['V'] = r[v_col].apply(lambda x: x.strip('()'))
-    r['J'] = r[j_col].apply(lambda x: x.strip('()'))
+    r['V'] = r[r.columns[1]].apply(lambda x: x.strip('()'))
+    r['J'] = r[r.columns[2]].apply(lambda x: x.strip('()'))
     s['sequence'] = s['nt_sequence']
     s['index'] = s['seq_index']
 
@@ -72,7 +63,7 @@ def set_counts(df):
     return df
 
 
-def make_rep(model, size, dir, mutation_rate=0.05, allelic_rate=0.5):
+def make_rep(model, size, dir, template, mutation_rate=0.05):
     if model.startswith('bcr'):
         # Here BCR repertoire is made up of 0.5 singletons, 0.5 * 1/5 tree roots and 0.5 * 4/5 tree nodes
         # Then the repertoire will be downsampled to just a half
@@ -82,38 +73,16 @@ def make_rep(model, size, dir, mutation_rate=0.05, allelic_rate=0.5):
         # TCR repertoire is just halved after downsampling
         n = size * 2
 
-    # Generate non-allelic initial repertoire
+    # Generate initial repertoire
     try:
-        run_igor(size=int(round(n * (1 - allelic_rate))),
+        run_igor(size=n,
                  model=model,
                  dir=dir,
-                 batch='default')
+                 template=template)
     except IgorError:
         raise
 
-    rep_default = read_igor_output(dir + 'default_generated/', model)
-    rep_default['descr'] = ['default'] * len(rep_default.index)
-    print('default')
-    print(len(rep_default.index))
-
-    # Generate allelic initial repertoire
-    try:
-        run_igor(size = int(round(n * allelic_rate)),
-                 model = model,
-                 dir = dir,
-                 batch = 'allelic')
-    except IgorError:
-        raise
-
-    rep_allelic = read_igor_output(dir + 'allelic_generated/', model)
-    rep_allelic['descr'] = ['allelic'] * len(rep_allelic.index)
-    print('allelic')
-    print(len(rep_allelic.index))
-
-    # Copying the second df to ignore corrections made during merging 
-    rep = merge_repertoires(rep_default, deepcopy(rep_allelic), ['index'])
-    print('after merging')
-    print(len(rep.index))
+    rep = read_igor_output(dir + 'generated/')
 
     # Introduce SHMs and simulate cell proliferation
     if args.model.startswith('bcr'):
@@ -131,7 +100,7 @@ def write_fasta(df):
 
     for i in range(len(df.index)):
         for j in range(df['clone_count'][i]):
-            lines.append('>' + df['descr'][i] + '_clone_' + str(df['index'][i]) + '_' + str(j) + '\n' + df['sequence'][i] + '\n')
+            lines.append('>' + 'clone_' + str(df['index'][i]) + '_' + str(j) + '\n' + df['sequence'][i] + '\n')
 
     with open(args.dir+'clones.fasta', 'w') as f:
         f.write(''.join(lines))
@@ -169,6 +138,8 @@ def run_mixcr(dir):
     call(["mixcr", "assemble",
           "-t", "50",
           '-OassemblingFeatures="[FR1+CDR1+FR2+CDR2+FR3+CDR3+FR4]"',
+          "-OcloneClusteringParameters=null",
+          "-OmaxBadPointsPercent=0",
           "--index", dir+"mixcr/index",
           dir+"mixcr/alignments.vdjca",
           dir+"mixcr/clones.clns"])
@@ -183,14 +154,14 @@ def run_mixcr(dir):
 
 parser = argparse.ArgumentParser(description="Simulate B or T cell receptor repertoire")
 parser.add_argument("-m", dest="model", type=str, help='Name of the model (bcr_heavy/tcr_alpha)', default='bcr_heavy')
-parser.add_argument("-a", dest="allelic_rate", type=float, help='Share of allelic variants (0.5 for heterozygosity)', default=0.5)
 parser.add_argument("-n", dest="size", type=int, help='Approximate size of the repertoire (minimal)')
 parser.add_argument("-d", dest="dir", type=str, help='Output directory')
 parser.add_argument("-mr", dest="mutation_rate", type=float, help='Bulk mutation rate')
 parser.add_argument("-s", dest="sequencing_mode", type=str, help='Type of sequencing data (MiSeq/HiSeq/RNASeq')
+parser.add_argument("-t", dest="template", type=str, help='Path to fasta file with genomic templates for IGoR')
 args = parser.parse_args()
 
-rep = make_rep(args.model, args.size, args.dir, args.mutation_rate)
+rep = make_rep(args.model, args.size, args.dir, args.template, args.mutation_rate)
 print('Repertoire is generated')
 rep.to_csv(args.dir + 'clones_init.csv', sep='\t', index=False)
 write_fasta(rep)
